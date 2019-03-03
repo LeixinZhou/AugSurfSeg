63a572d2a1453602c7971c1307992708f1108cf3
leixinzhou <llasionzhou@gmail.com>
2019-03-02 19:07:07 -0600

    add circulateud

--- rigidTrans.py
+++ rigidTrans.py
@@ -28,7 +28,7 @@ class MirrorUD(AugWithGTChange):
         return img_nparray.shape[0] - gt_nparray - 1
 
 
-class CirculateCol(AugWithGTChange):
+class CirculateLR(AugWithGTChange):
     def img_aug(self, input_img_gt, shift_step):
         img_nparray = input_img_gt['img']
         aug_img_nparray = np.empty_like(img_nparray)
@@ -50,3 +50,30 @@ class CirculateCol(AugWithGTChange):
         shift_step = random.randint(1, input_img_gt['gt'].shape[0]-1)
         return {'img': self.img_aug(input_img_gt, shift_step),
                 'gt': self.gt_aug(input_img_gt, shift_step)}
+
+
+class CirculateUD(AugWithGTChange):
+    def img_aug(self, img_nparray, shift_step):
+        aug_img_nparray = np.empty_like(img_nparray)
+        aug_img_nparray[:shift_step, ] = img_nparray[-shift_step:, ]
+        aug_img_nparray[shift_step:, ] = img_nparray[:-shift_step, ]
+        return aug_img_nparray
+
+    def gt_aug(self, gt_nparray, shift_step, row_len):
+        aug_gt_nparray = (gt_nparray + shift_step) % row_len
+        return aug_gt_nparray
+
+    def __call__(self, input_img_gt):
+        """
+        This function circulates image and gt down with a random step number.
+        """
+        gt_nparray = input_img_gt['gt']
+        img_nparray = input_img_gt['img']
+        gt_max, gt_min = np.max(gt_nparray), np.min(gt_nparray)
+        row_len = img_nparray.shape[0]
+        if row_len - gt_max > gt_min:
+            shift_step = random.randint(1, (row_len-gt_max)//2)
+        else:
+            shift_step = random.randint(-gt_min//2, -1) + row_len
+        return {'img': self.img_aug(img_nparray, shift_step),
+                'gt': self.gt_aug(gt_nparray, shift_step, row_len)}
